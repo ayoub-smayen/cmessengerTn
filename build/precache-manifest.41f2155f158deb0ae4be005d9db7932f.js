9b5b1931ab903324aa15f16d178d16f9ac85a75b
ayoub-smayen <ayoubjobs.2019@gmail.com>
2021-07-17 20:44:40 +0200

    'Removenode_modules2221k'

--- build/precache-manifest.41f2155f158deb0ae4be005d9db7932f.js
+++ build/precache-manifest.41f2155f158deb0ae4be005d9db7932f.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ad763f606d10fa5664bdf206d8b7ae8e",
+    "revision": "50fe0c18a660d3d466f6bdfac3c64b9f",
     "url": "/index.html"
   },
   {
-    "revision": "3aa57be0590cc322e1b4",
+    "revision": "d8eaeeea44ead1e9a3f0",
     "url": "/static/css/2.0b0521d5.chunk.css"
   },
   {
-    "revision": "d8a4735110b1a3e2cf03",
+    "revision": "0cd885eec06ca05bb3b6",
     "url": "/static/css/main.7cce6914.chunk.css"
   },
   {
-    "revision": "3aa57be0590cc322e1b4",
-    "url": "/static/js/2.ed7b38f9.chunk.js"
+    "revision": "d8eaeeea44ead1e9a3f0",
+    "url": "/static/js/2.dafdf22c.chunk.js"
   },
   {
     "revision": "57f3760633750f6b526a069401b83b41",
-    "url": "/static/js/2.ed7b38f9.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.dafdf22c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d8a4735110b1a3e2cf03",
-    "url": "/static/js/main.e2237d31.chunk.js"
+    "revision": "0cd885eec06ca05bb3b6",
+    "url": "/static/js/main.6a30092c.chunk.js"
   },
   {
     "revision": "d9456f738a09fd8978f0",
